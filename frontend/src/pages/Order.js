ba753397d095958f8a5f6874306c6e95fd933301
Steven Nguyen <stevinhnguyen@gmail.com>
2023-05-02 22:48:06 -0700

    Fixed

--- frontend/src/pages/Order.js
+++ frontend/src/pages/Order.js
@@ -140,7 +140,7 @@ function PlaceOrder() {
                           />
                         </Col>
                         <Col>
-                          <Link to={`/product/${item._id}`}>{item.name}</Link>
+                          <Link to={`/product/${index + 1}`}>{item.name}{console.log(index+1)}</Link>
                         </Col>
                         <Col md={4}>
                           {item.quantity} X ${item.price} = $
